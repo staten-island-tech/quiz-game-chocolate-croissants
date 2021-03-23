0e0fc9f40223a8294ec130cd05d73a300365df21
langerdaniel9 <langerdaniel19@gmail.com>
2021-03-23 12:46:33 -0400

    super basic js setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,79 @@
+const questions = [
+  {
+    question: "What is the square root of pi?",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+  },
+  {
+    question: "Donald Trump",
+    choice1: "1",
+    choice2: "R",
+    choice3: "R",
+    choice4: "R",
+  },
+  {
+    question: "Donald Trump",
+    choice1: "1",
+    choice2: "R",
+    choice3: "R",
+    choice4: "R",
+  },
+  {
+    question: "Donald Trump",
+    choice1: "1",
+    choice2: "R",
+    choice3: "R",
+    choice4: "R",
+  },
+  {
+    question: "Donald Trump",
+    choice1: "1",
+    choice2: "R",
+    choice3: "R",
+    choice4: "R",
+  },
+  {
+    question: "Donald Trump",
+    choice1: "1",
+    choice2: "R",
+    choice3: "R",
+    choice4: "R",
+  },
+  {
+    question: "Donald Trump",
+    choice1: "1",
+    choice2: "R",
+    choice3: "R",
+    choice4: "R",
+  },
+  {
+    question: "Donald Trump",
+    choice1: "1",
+    choice2: "R",
+    choice3: "R",
+    choice4: "R",
+  },
+  {
+    question: "Donald Trump",
+    choice1: "1",
+    choice2: "R",
+    choice3: "R",
+    choice4: "R",
+  },
+  {
+    question: "Donald Trump",
+    choice1: "1",
+    choice2: "R",
+    choice3: "R",
+    choice4: "R",
+  },
+];
+
 const btn = document.getElementById("next-btn");
 const question = document.getElementById("question");
 
 btn.onclick = function () {
-  question.innerHTML = "test";
+  question.innerHTML = questions[0].question;
 };
