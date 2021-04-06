7d7266fb4dcc684edfd9cf5c2427216f1467ca3e
langerdaniel9 <langerdaniel19@gmail.com>
2021-04-05 20:06:08 -0400

    updoot

--- src/js/index.js
+++ src/js/index.js
@@ -71,9 +71,44 @@ const questions = [
   },
 ];
 
-const btn = document.getElementById("next-btn");
-const question = document.getElementById("question");
+const questionFormat = `
+<div class='main-center'>
+  <div class='top80'>
+    <h2 class='question' id='question'></h2>
+    <div class='choices'>
+      <div class='choice-a'>
+        <input class='choices-btn choice-1' name='1' type='radio' value='A'>
+        <label></label>
+      </div>
+      <div class='choice-b'>
+        <input class='choices-btn choice-2' name='1' type='radio' value='B'>
+        <label></label>
+      </div>
+      <div class='choice-c'>
+        <input class='choices-btn choice-3' name='1' type='radio' value='C'>
+        <label></label>
+      </div>
+      <div class='choice-d'>
+        <input class='choices-btn choice-4' name='1' type='radio' value='D'>
+        <label></label>
+      </div>
+    </div>
+  </div>
+  <div class='bottom'>
+    <div class='back'>
+      <button class='back-btn' id='back-btn'>Back</button>
+    </div>
+    <div class='next'>
+        <button class='next-btn' id='next-btn'>Next</button>
+    </div>
+  </div>
+</div>
+`;
 
-btn.onclick = function () {
-  question.innerHTML = questions[0].question;
+const start = document.getElementById("startbtn");
+
+start.onclick = function () {
+  console.log("click");
+  document.getElementById("main").innerHTML += questionFormat;
+  console.log("2");
 };
