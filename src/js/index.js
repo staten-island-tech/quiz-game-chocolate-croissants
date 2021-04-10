707ee1ec7c13481672d7c5050771f55db3e1d943
langerdaniel9 <langerdaniel19@gmail.com>
2021-04-10 19:29:21 -0400

    yup

--- src/js/index.js
+++ src/js/index.js
@@ -1,73 +1,83 @@
 const questions = [
   {
-    question: "What is the square root of pi?",
+    question: "Question 1",
     choice1: "A",
     choice2: "B",
     choice3: "C",
     choice4: "D",
+    correct: "",
   },
   {
-    question: "Donald Trump",
-    choice1: "1",
-    choice2: "R",
-    choice3: "R",
-    choice4: "R",
+    question: "Question 2",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    correct: "",
   },
   {
-    question: "Donald Trump",
-    choice1: "1",
-    choice2: "R",
-    choice3: "R",
-    choice4: "R",
+    question: "Question 3",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    correct: "",
   },
   {
-    question: "Donald Trump",
-    choice1: "1",
-    choice2: "R",
-    choice3: "R",
-    choice4: "R",
+    question: "Question 4",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    correct: "",
   },
   {
-    question: "Donald Trump",
-    choice1: "1",
-    choice2: "R",
-    choice3: "R",
-    choice4: "R",
+    question: "Question 5",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    correct: "",
   },
   {
-    question: "Donald Trump",
-    choice1: "1",
-    choice2: "R",
-    choice3: "R",
-    choice4: "R",
+    question: "Question 6",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    correct: "",
   },
   {
-    question: "Donald Trump",
-    choice1: "1",
-    choice2: "R",
-    choice3: "R",
-    choice4: "R",
+    question: "Question 7",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    correct: "",
   },
   {
-    question: "Donald Trump",
-    choice1: "1",
-    choice2: "R",
-    choice3: "R",
-    choice4: "R",
+    question: "Question 8",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    correct: "",
   },
   {
-    question: "Donald Trump",
-    choice1: "1",
-    choice2: "R",
-    choice3: "R",
-    choice4: "R",
+    question: "Question 9",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    correct: "",
   },
   {
-    question: "Donald Trump",
-    choice1: "1",
-    choice2: "R",
-    choice3: "R",
-    choice4: "R",
+    question: "Question 10",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    correct: "",
   },
 ];
 
@@ -78,25 +88,25 @@ const questionFormat = `
     <div class='choices'>
       <div class='choice-a'>
         <input class='choices-btn choice-1' name='1' type='radio' value='A'>
-        <label></label>
+        <label id="choice1"></label>
       </div>
       <div class='choice-b'>
         <input class='choices-btn choice-2' name='1' type='radio' value='B'>
-        <label></label>
+        <label id="choice2"></label>
       </div>
       <div class='choice-c'>
         <input class='choices-btn choice-3' name='1' type='radio' value='C'>
-        <label></label>
+        <label id="choice3"></label>
       </div>
       <div class='choice-d'>
         <input class='choices-btn choice-4' name='1' type='radio' value='D'>
-        <label></label>
+        <label id="choice4"></label>
       </div>
     </div>
   </div>
   <div class='bottom'>
     <div class='back'>
-      <button class='back-btn' id='back-btn'>Back</button>
+      <button class='exit-btn' id='exit-btn'>Exit</button>
     </div>
     <div class='next'>
         <button class='next-btn' id='next-btn'>Next</button>
@@ -104,11 +114,38 @@ const questionFormat = `
   </div>
 </div>
 `;
+const numberQuestions = questions.length;
+let number = numberQuestions;
+const mainButton = document.getElementById("main-btn");
+const exitButton = document.getElementById("exit-button");
 
-const start = document.getElementById("startbtn");
+mainButton.onclick = function () {
+  const top80 = document.getElementById("top80");
 
-start.onclick = function () {
-  console.log("click");
-  document.getElementById("main").innerHTML += questionFormat;
-  console.log("2");
+  const title = document.getElementById("main-title");
+  const question = document.getElementById("question");
+  const choice1 = document.getElementById("choice1");
+  const choice2 = document.getElementById("choice2");
+  const choice3 = document.getElementById("choice3");
+  const choice4 = document.getElementById("choice4");
+
+  const nextButton = document.getElementById("next-button");
+
+  number -= 1;
+  if (number == numberQuestions) {
+    mainButton.innerHTML = "Start";
+  } else if (number < numberQuestions && number > 0) {
+    mainButton.innerHTML = "Next";
+    top80.style.opacity = "1";
+  } else if (number == 0) {
+    mainButton.innerHTML = "Grade Quiz";
+  } else {
+    mainButton.setAttribute("disabled", "true");
+  }
+
+  //startButton.insertAdjacentHTML("beforebegin", questionFormat);
+};
+
+exitButton.onclick = function () {
+  console.log("we quitting");
 };
