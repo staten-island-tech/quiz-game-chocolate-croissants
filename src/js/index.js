7ae75f01ed3e46ae2f93eb2f119f8e246b56557f
langerdaniel9 <langerdaniel19@gmail.com>
2021-04-11 04:06:08 -0400

    cleaning up JS

--- src/js/index.js
+++ src/js/index.js
@@ -83,7 +83,6 @@ const questions = [
 
 let numCorrect = 0;
 let questionNumber = 0;
-const numberQuestions = questions.length;
 const mainButton = document.getElementById("main-btn");
 const exitButton = document.getElementById("exit-btn");
 
@@ -94,7 +93,6 @@ function insertQuestionVars(qNum) {
   const choice2 = document.getElementById("choice2");
   const choice3 = document.getElementById("choice3");
   const choice4 = document.getElementById("choice4");
-
   title.innerText = `Question Number ${qNum + 1}`;
   question.innerText = questions[qNum].question;
   choice1.innerText = questions[qNum].choice1;
@@ -112,7 +110,6 @@ function findWhichChoiceIsSelected(num) {
   const c2 = document.getElementsByName("1")[1];
   const c3 = document.getElementsByName("1")[2];
   const c4 = document.getElementsByName("1")[3];
-
   if (c1.checked == true) {
     c1.checked = false;
     if ("A" == questions[num].correct) {
@@ -138,43 +135,31 @@ function findWhichChoiceIsSelected(num) {
 
 mainButton.addEventListener("click", function () {
   const top80 = document.getElementById("top80");
-  console.log(questionNumber);
-
+  const numberQuestions = questions.length;
   if (questionNumber == 0) {
     mainButton.innerHTML = "Next";
     top80.style.opacity = "1";
     insertQuestionVars(questionNumber);
     questionNumber += 1;
-
-    ///////////////////////////////////////////////
   } else if (questionNumber < numberQuestions - 1) {
     findWhichChoiceIsSelected(questionNumber - 1);
-    console.log("correct = ", numCorrect);
-
     insertQuestionVars(questionNumber);
-
     questionNumber += 1;
-
-    ///////////////////////////////////////////////
   } else if (questionNumber == numberQuestions - 1) {
     mainButton.innerHTML = "Grade Quiz";
     findWhichChoiceIsSelected(questionNumber - 1);
-    console.log("correct = ", numCorrect);
     insertQuestionVars(questionNumber);
     questionNumber += 1;
-    ///////////////////////////////////////////////
   } else if (questionNumber == numberQuestions) {
     const top60 = document.getElementById("choices");
     const title = document.getElementById("main-title");
     mainButton.innerHTML = "Grade Quiz";
     findWhichChoiceIsSelected(questionNumber - 1);
-    console.log("correct = ", numCorrect);
     questionNumber += 1;
     top60.style.opacity = "0";
     title.innerText = "You finished the Quiz";
     question.innerText = `You got ${numCorrect}/${numberQuestions}`;
     mainButton.innerHTML = "Retake";
-    ///////////////////////////////////////////////
   } else {
     location.reload();
   }
